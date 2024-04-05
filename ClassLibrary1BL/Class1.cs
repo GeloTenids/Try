1e836ec0eaeebca8d1108fca185db9b7a0f278ca
GeloTenids <matenido@gmail.com>
2024-04-05 19:40:58 +0800

    Add project files.

---
+++ ClassLibrary1BL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary1BL
+{
+    public class Class1
+    {
+        public bool Hello() 
+        {
+            return true;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibrary1BL;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 hel = new Class1();
+            Console.WriteLine(hel.Hello());
+        }
+    }
+}
